8cb663ff4e013bb554310f8a773f3cc9621332aa
camilajesus <camilaajjesus@gmail.com>
2017-06-15 18:18:48 -0300

    Add project files.

---
+++ Pomodori/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Pomodori
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.tbParcial = new System.Windows.Forms.TextBox();
+            this.tmrCronometro = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempo.Location = new System.Drawing.Point(21, 47);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(216, 42);
+            this.lblTempo.TabIndex = 1;
+            this.lblTempo.Text = "00:00:00:00";
+            this.lblTempo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnParar
+            // 
+            this.btnParar.Enabled = false;
+            this.btnParar.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParar.Location = new System.Drawing.Point(12, 202);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(242, 58);
+            this.btnParar.TabIndex = 5;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(12, 138);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(242, 58);
+            this.btnIniciar.TabIndex = 4;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // tbParcial
+            // 
+            this.tbParcial.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbParcial.Location = new System.Drawing.Point(260, 12);
+            this.tbParcial.Multiline = true;
+            this.tbParcial.Name = "tbParcial";
+            this.tbParcial.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbParcial.Size = new System.Drawing.Size(222, 248);
+            this.tbParcial.TabIndex = 6;
+            this.tbParcial.TextChanged += new System.EventHandler(this.tbParcial_TextChanged);
+            // 
+            // tmrCronometro
+            // 
+            this.tmrCronometro.Enabled = true;
+            this.tmrCronometro.Interval = 1;
+            this.tmrCronometro.Tick += new System.EventHandler(this.tmrCronometro_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 270);
+            this.Controls.Add(this.tbParcial);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lblTempo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pomodori";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox tbParcial;
+        private System.Windows.Forms.Timer tmrCronometro;
+    }
+}
+
---
+++ Pomodori/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Pomodori
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch pomodori = new Stopwatch();
+        int vezParcial = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            //zerar o cronometro
+            pomodori.Reset();
+
+            //iniciar o cronometro
+            pomodori.Start();
+
+            //zerar as variaveis
+            tbParcial.Text = null;
+            vezParcial = 0;
+
+            //desativar botão iniciar e ativar os de parada
+            btnParar.Enabled = true;
+            btnIniciar.Enabled = false;
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            //para o cronômetro
+            pomodori.Stop();
+
+            //desativar botões de paradas e ativar o de início
+            btnParar.Enabled = false;
+            btnIniciar.Enabled = true;
+        }
+
+        private void tbParcial_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrCronometro_Tick(object sender, EventArgs e)
+        {
+            lblTempo.Text = String.Format("{0:00}:{1:00}:{2:00}", pomodori.Elapsed.Hours, pomodori.Elapsed.Minutes, pomodori.Elapsed.Seconds, pomodori.Elapsed.Milliseconds / 10);
+            //:{3:00}
+
+            if (lblTempo.Text == String.Format("{0:00}:{0:00}:{0:15}", pomodori.Elapsed.Hours, pomodori.Elapsed.Minutes, pomodori.Elapsed.Seconds, pomodori.Elapsed.Milliseconds / 10))
+            {
+                tbParcial.Text = "Hora da 1ºpausa" + Environment.NewLine;
+                //para o cronômetro
+                pomodori.Stop();
+                pomodori.Start();
+            }
+
+
+            else if (lblTempo.Text == string.Format("{0:00}:{0:00}:{0:30}", pomodori.Elapsed.Hours, pomodori.Elapsed.Minutes, pomodori.Elapsed.Seconds, pomodori.Elapsed.Milliseconds / 10))
+            {
+                pomodori.Stop();
+                tbParcial.Text = "Final da pausa";
+                pomodori.Reset();
+                pomodori.Stop();
+                pomodori.Start();
+            }
+
+            else if (lblTempo.Text == String.Format("{0:00}:{0:00}:{0:45}", pomodori.Elapsed.Hours, pomodori.Elapsed.Minutes, pomodori.Elapsed.Seconds, pomodori.Elapsed.Milliseconds / 10))
+            {
+                tbParcial.Text = "Hora da 2ºpausa" + Environment.NewLine;
+                //para o cronômetro
+                pomodori.Stop();
+                pomodori.Start();
+                pomodori.Reset();
+                pomodori.Stop();
+            }
+        }
+    }
+}
